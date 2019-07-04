cd0577b88a9e586b0c3fd6e52dce62dba5169e8f
Patrick Battisti <1729patrick@gmail.com>
2019-07-03 21:50:55 -0300

    Modulo 2

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '615D3CD5455EBB87E70F68C93F5393E1',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const router = new Router();
+
+router.post('/sessions', SessionController.store);
+router.post('/users', UserController.store);
+
+router.use(authMiddleware);
+
+router.put('/users', UserController.update);
+
+export default router;
