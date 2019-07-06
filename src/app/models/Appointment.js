b3b9ef39bd3540ad681d9862814d2dd93c8f882c
Patrick Battisti <1729patrick@gmail.com>
2019-07-06 19:19:22 -0300

    Agendamento de serviço

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -5,7 +5,7 @@ class Appointment extends Model {
     super.init(
       {
         date: Sequelize.DATE,
-        canceled_id: Sequelize.date,
+        canceled_id: Sequelize.DATE,
       },
       {
         sequelize,
@@ -15,7 +15,7 @@ class Appointment extends Model {
     return this;
   }
 
-  static associeate(models) {
+  static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,7 +20,10 @@ router.use(authMiddleware);
 
 router.put('/users', UserController.update);
 
-router.post('/files', upload.single('file'), FileController.store);
 router.get('/providers', ProviderController.index);
+
+router.post('/appointments', AppointmentController.store);
+
+router.post('/files', upload.single('file'), FileController.store);
 
 export default router;
